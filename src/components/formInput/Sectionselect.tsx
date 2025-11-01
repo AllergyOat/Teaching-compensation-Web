114a2a3f745e7730e882d1815042bac0555ee24a
AllergyOat <apichat.peanroop@gmail.com>
2025-11-01 20:55:11 +0700

    fix wrong section select

--- src/components/formInput/Sectionselect.tsx
+++ src/components/formInput/Sectionselect.tsx
@@ -1,31 +1,35 @@
-import { Card, CardContent } from '../ui/card'
-import SpecialBadge from './SpecialBadge'
-import SectionBadge from './SectionBadge'
-import { Link } from 'react-router'
+import { Card, CardContent } from "../ui/card";
+import SpecialBadge from "./SpecialBadge";
+import SectionBadge from "./SectionBadge";
+import { Link } from "react-router";
 
 const Sectionselect = ({ name }: { name: string }) => {
-  // กำหนด Badge component ตามชื่อ
   const BadgeComponent = name === "ภาคปกติ" ? SectionBadge : SpecialBadge;
-  
-  return (
-    <div className='mb-4'>
-          <p className="font-bold">{name}</p>
-          <Link to="/form/new?program=SPECIAL_PROGRAM&section=LECTURE">
-            <Card className="mt-2 h-16 w-full cursor-pointer border-0 shadow-md transition-all duration-200 hover:scale-[1.02] hover:bg-gray-100 hover:shadow-lg">
-              <CardContent className="flex h-full items-center">
-                <p>กรอกแบบฟอร์มการสอนหมู่บรรยาย <BadgeComponent /></p>
-              </CardContent>
-            </Card>
-          </Link>
-          <Link to="/form/new?program=SPECIAL_PROGRAM&section=LAB">
-            <Card className="mt-4 h-16 w-full cursor-pointer border-0 shadow-md transition-all duration-200 hover:scale-[1.02] hover:bg-gray-100 hover:shadow-lg">
-              <CardContent className="flex h-full items-center">
-                <p>กรอกแบบฟอร์มการสอนหมู่ปฏิบัติ <BadgeComponent /></p>
-              </CardContent>
-            </Card>
-          </Link>
-    </div>
-  )
-}
+  const program = name === "ภาคปกติ" ? "REGULAR_PROGRAM" : "SPECIAL_PROGRAM";
 
-export default Sectionselect
+  return (
+    <div className="mb-4">
+      <p className="font-bold">{name}</p>
+      <Link to={`/form/new?program=${program}&section=LECTURE`}>
+        <Card className="mt-2 h-16 w-full cursor-pointer border-0 shadow-md transition-all duration-200 hover:scale-[1.02] hover:bg-gray-100 hover:shadow-lg">
+          <CardContent className="flex h-full items-center">
+            <p>
+              กรอกแบบฟอร์มการสอนหมู่บรรยาย <BadgeComponent />
+            </p>
+          </CardContent>
+        </Card>
+      </Link>
+      <Link to={`/form/new?program=${program}&section=LAB`}>
+        <Card className="mt-4 h-16 w-full cursor-pointer border-0 shadow-md transition-all duration-200 hover:scale-[1.02] hover:bg-gray-100 hover:shadow-lg">
+          <CardContent className="flex h-full items-center">
+            <p>
+              กรอกแบบฟอร์มการสอนหมู่ปฏิบัติ <BadgeComponent />
+            </p>
+          </CardContent>
+        </Card>
+      </Link>
+    </div>
+  );
+};
+
+export default Sectionselect;
